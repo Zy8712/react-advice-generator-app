b410fe5c7d37bc2331884b210b5d4d263e2c1e6c
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2023-08-15 00:31:20 -0400

    Update DiceButton.js

--- src/DiceButton.js
+++ src/DiceButton.js
@@ -1,15 +1,18 @@
 import diceIcon from './assets/icon-dice.svg';
 import './App.css';
 
-function DiceButton(){
-    return(
-        <>
+function DiceButton({ onClick, disabled }) {
+    return (
         <div className="w-full absolute -bottom-16 block flex justify-center">
-            <button id="random-advice-button" className="flex flex-col justify-center items-center rounded-full p-5 border-none">
+            <button
+                id="random-advice-button"
+                className="flex flex-col justify-center items-center rounded-full p-5 border-none"
+                onClick={onClick} // Pass the onClick prop to the button
+                disabled={disabled} // Pass the disabled prop to the button
+            >
                 <img src={diceIcon} alt="dice_icon" />
             </button>
         </div>
-        </>
     );
 }
 
