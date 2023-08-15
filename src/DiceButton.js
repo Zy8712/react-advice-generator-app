10197389f79f00c163af368d483a7f5e4945670b
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2023-08-15 00:33:21 -0400

    Update DiceButton.js

--- src/DiceButton.js
+++ src/DiceButton.js
@@ -1,16 +1,19 @@
+import React, { useRef } from 'react';
 import diceIcon from './assets/icon-dice.svg';
 import './App.css';
 
 function DiceButton({ onClick, disabled }) {
+    const diceIconRef = useRef(null); // Create a useRef for the dice icon
+
     return (
         <div className="w-full absolute -bottom-16 block flex justify-center">
             <button
                 id="random-advice-button"
                 className="flex flex-col justify-center items-center rounded-full p-5 border-none"
-                onClick={onClick} // Pass the onClick prop to the button
-                disabled={disabled} // Pass the disabled prop to the button
+                onClick={onClick}
+                disabled={disabled}
             >
-                <img src={diceIcon} alt="dice_icon" />
+                <img ref={diceIconRef} src={diceIcon} alt="dice_icon" /> {/* Attach the ref */}
             </button>
         </div>
     );
